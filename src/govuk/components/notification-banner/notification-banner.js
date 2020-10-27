306d1710e8c44444bab63aec6cc6f3807da75f94
Hanna Laakso <hanna.laakso@digital.cabinet-office.gov.uk>
2020-10-27 16:40:56 +0000

    Simplify auto-focus functionality

--- src/govuk/components/notification-banner/notification-banner.js
+++ src/govuk/components/notification-banner/notification-banner.js
@@ -19,51 +19,31 @@ NotificationBanner.prototype.init = function () {
 /**
  * Focus the element
  *
- * Move focus to the element to make assistive technologies prioritise announcing it, in addition
- * to setting `role="alert"` in the HTML (both `role="alert"` and `data-auto-focus="true"`are
- * required for the auto-focus functionality to activate).
- * *
- * This function should be called *after* the DOMContentLoaded event is fired.
+ * If `role="alert"` is set, focus the element to help some assistive technologies
+ * prioritise announcing it.
  *
- * You can turn off the auto-focus functionality by removing the `data-auto-focus` attribute
- * from the component HTML. You might wish to do this based on user research findings, or to avoid
+ * This function should be called *after* the `DOMContentLoaded` event is fired.
+ *
+ * You can turn off the auto-focus functionality by setting `data-disable-auto-focus="true"` in the * component HTML. You might wish to do this based on user research findings, or to avoid
  * a clash with another element which should be focused when the page loads.
  */
 NotificationBanner.prototype.setFocus = function () {
   var $module = this.$module
 
-  // Check if the attribute that determines whether the element should be auto-focused is present
-  // Check if role="alert" is set
-  if ($module.getAttribute('data-auto-focus') !== 'true' || $module.getAttribute('role') !== 'alert') {
+  // Check if the attribute that disables the auto focus is present
+  if ($module.getAttribute('data-disable-auto-focus') === 'true') {
     return
   }
 
-  // When there are multiple notification banners present, the most relevant one should be placed
-  // at the top.
-  // Therefore focus the first banner on the page.
-  // Check also that the page doesn't contain an error summary since an error summary should receive
-  // focus instead of a notification banner.
-  if (this.checkIfAnotherElementFocused()) return
+  // Check if `role="alert"` is set. When this role is added to an element, the browser will send out an accessible alert event to assistive technology products which can then notify the user about it.
+  if ($module.getAttribute('role') !== 'alert') {
+    return
+  }
+
+  // Set tabindex to -1 so to make the element focusable with JavaScript (the element will not be reachable via sequential keyboard navigation).
+  $module.setAttribute('tabindex', '-1')
 
   this.$module.focus()
-}
-
-/**
- * Check for other focused elements on the page
- *
- * Prevent auto-focusing the notification banner if there is another notification banner on the page which is already focused, or an error summary.
- *
- * */
-NotificationBanner.prototype.checkIfAnotherElementFocused = function () {
-  if (!document.activeElement) {
-    return
-  }
-
-  var errorSummaryPresent = document.querySelector('[data-module="govuk-error-summary"]')
-
-  var anotherNotificationFocused = document.activeElement.classList.contains('govuk-notification-banner')
-
-  return errorSummaryPresent || anotherNotificationFocused
 }
 
 export default NotificationBanner
